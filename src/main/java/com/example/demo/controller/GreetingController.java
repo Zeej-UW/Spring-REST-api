e91b562ea28e2f171b049af211b1faaedd57532c
Charles Jackson <charlesjackson3945@gmail.com>
2022-02-07 19:57:38 -0800

    Init Spring Boot API Greetings project

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Greeting;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @PostMapping("/greeting")
+    public void postGreeting(@RequestBody Greeting greeting) {
+        System.out.println(greeting.toString());
+    }
+}
